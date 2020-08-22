a375d26b1e988148d85b1da064b347d312a75f45
Lars Klemstein <lklemstein@axway.com>
2020-08-22 18:16:41 +0200

    added rust installer

--- install_toolchain.bash
+++ install_toolchain.bash
@@ -38,49 +38,49 @@ cd $TMPDIR
 # ----------------------------------------------------------------------------
 
 main() {
-	check_requirements
-    initial_cleanup
+    check_requirements
+    # initial_cleanup
 
-	# install_homebrew
+    # install_homebrew
 
-	# set_homebrew_environment
-	# install_homebrew_packages
-	# install_python_packages
+    # set_homebrew_environment
+    # install_homebrew_packages
+    # install_python_packages
 
-	install_rust
+    install_rust
 
-	msg_ok "Done!"
-	exit 0
+    msg_ok "Done!"
+    exit 0
 }
 
 msg_ok() {
-	echo -e "\e[92m[$PROG] $1\e[39m" >&2
+    echo -e "\e[92m[$PROG] $1\e[39m" >&2
 }
 
 msg_warn() {
-	echo -e "\e[93m[$PROG] $1\e[39m" >&2
+    echo -e "\e[93m[$PROG] $1\e[39m" >&2
 }
 
 msg_error() {
-	echo -e "\e[91m[$PROG] $1\e[39m" >&2
+    echo -e "\e[91m[$PROG] $1\e[39m" >&2
 }
 
 msg_abort() {
-	msg_error "$1"
-	exit 1
+    msg_error "$1"
+    exit 1
 }
 
 check_requirements() {
-	test -f $EXPECT_SCRIPT || \
-		msg_abort "expect script \"$EXPECT_SCRIPT\" not found"
-	test -x $EXPECT_SCRIPT || \
-		msg_abort "expect script \"$EXPECT_SCRIPT\" found, but not executable"
+    test -f $EXPECT_SCRIPT || \
+        msg_abort "expect script \"$EXPECT_SCRIPT\" not found"
+    test -x $EXPECT_SCRIPT || \
+        msg_abort "expect script \"$EXPECT_SCRIPT\" found, but not executable"
 
-	test -f $HOMEBREW_PACKAGES || \
-		msg_abort "homebrew packages file \"$HOMEBREW_PACKAGES\" not found"
+    test -f $HOMEBREW_PACKAGES || \
+        msg_abort "homebrew packages file \"$HOMEBREW_PACKAGES\" not found"
 
-	test -f $PYTHON_PACKAGES || \
-		msg_abort "python packages file \"$PYTHON_PACKAGES\" not found"
+    test -f $PYTHON_PACKAGES || \
+        msg_abort "python packages file \"$PYTHON_PACKAGES\" not found"
 }
 
 initial_cleanup() {
@@ -97,70 +97,83 @@ initial_cleanup() {
 }
 
 install_homebrew() {
-	local installer_basename user_name marker
+    local installer_basename user_name marker
 
-	marker='[Homebrew install] '
-	msg_ok "${marker}now in $PWD"
-	msg_ok "${marker}download $HOMEBREW_INSTALLER_URL..."
+    marker='[Homebrew install] '
+    msg_ok "${marker}now in $PWD"
+    msg_ok "${marker}download $HOMEBREW_INSTALLER_URL..."
 
-	installer_basename=${HOMEBREW_INSTALLER_URL##*/}
-	msg_ok "${marker}installer should be $PWD/$installer_basename"
+    installer_basename=${HOMEBREW_INSTALLER_URL##*/}
+    msg_ok "${marker}installer should be $PWD/$installer_basename"
 
-	user_name=$(id -un)
+    user_name=$(id -un)
 
-	wget -q $HOMEBREW_INSTALLER_URL
+    wget -q $HOMEBREW_INSTALLER_URL
 
-	chmod +x $installer_basename
-	msg_ok "chmod +x on $installer_basename"
+    chmod +x $installer_basename
+    msg_ok "chmod +x on $installer_basename"
 
-	msg_ok "${marker}call $EXPECT_SCRIPT to install..."
-	$EXPECT_SCRIPT ./$installer_basename $user_name
+    msg_ok "${marker}call $EXPECT_SCRIPT to install..."
+    $EXPECT_SCRIPT ./$installer_basename $user_name
 
-	msg_ok "${marker}...done"
+    msg_ok "${marker}...done"
 }
 
 set_homebrew_environment() {
-	msg_ok "Create/overwrite $PROFILE_HOMEBREW..."
-	$HOME/.linuxbrew/bin/brew shellenv >$PROFILE_HOMEBREW
+    msg_ok "Create/overwrite $PROFILE_HOMEBREW..."
+    $HOME/.linuxbrew/bin/brew shellenv >$PROFILE_HOMEBREW
 
-	if ! grep -q ". \$HOME/.${PROFILE_HOMEBREW}" $PROFILE_USER
-	then
-		{
-			echo ""
-			echo ". \$HOME/.${PROFILE_HOMEBREW}"
-		} >> $PROFILE_USER
+    if ! grep -q ". \$HOME/.${PROFILE_HOMEBREW}" $PROFILE_USER
+    then
+        {
+            echo ""
+            echo ". \$HOME/.${PROFILE_HOMEBREW}"
+        } >> $PROFILE_USER
 
-		msg_ok "Added sourcing of $PROFILE_HOMEBREW in $PROFILE_USER..."
-	else
-		msg_ok "Sourcing of $PROFILE_HOMEBREW in $PROFILE_USER already in place"
-	fi
+        msg_ok "Added sourcing of $PROFILE_HOMEBREW in $PROFILE_USER..."
+    else
+        msg_ok "Sourcing of $PROFILE_HOMEBREW in $PROFILE_USER already in place"
+    fi
 
-	. $PROFILE_HOMEBREW
-	msg_ok "Activated homebrew environment for further processing"
+    . $PROFILE_HOMEBREW
+    msg_ok "Activated homebrew environment for further processing"
 }
 
 install_homebrew_packages() {
-	msg_ok "Install Homebrew packages from $HOMEBREW_PACKAGES..."
+    msg_ok "Install Homebrew packages from $HOMEBREW_PACKAGES..."
 
-	for program in $(< $HOMEBREW_PACKAGES) 
-	do
-		msg_ok "-> $program"
-		brew install $program
-	done
+    for program in $(< $HOMEBREW_PACKAGES) 
+    do
+        msg_ok "-> $program"
+        brew install $program
+    done
 
-	msg_ok "Homebrew package installation finshed"
+    msg_ok "Homebrew package installation finshed"
 }
 
 install_python_packages() {
-	msg_ok "Install Python packages from $HOMEBREW_PACKAGES..."
+    msg_ok "Install Python packages from $HOMEBREW_PACKAGES..."
 
-	for py_package in $(< $PYTHON_PACKAGES) 
-	do
-		msg_ok "-> $py_package"
-		pip3 install $py_package
-	done
+    for py_package in $(< $PYTHON_PACKAGES) 
+    do
+        msg_ok "-> $py_package"
+        pip3 install $py_package
+    done
 
-	msg_ok "Python packages installation finshed"
+    msg_ok "Python packages installation finshed"
+}
+
+install_rust() {
+    local rustup_program=$(which rustup)
+    if [ -n "$rustup_program" ] && [[ "$rustup_program" == $HOME/* ]]
+    then
+        msg_warn "Found local rust installation, will only update..."
+        rustup update
+        msg_warn "...done"
+    else
+        msg_ok "Install rust..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    fi
 }
 
 
